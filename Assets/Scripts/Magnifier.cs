88e27b617220d29b8d693be00fa7cffd127c6329
CarrotAlice <karen.hormann@gmail.com>
2026-03-12 12:23:42 +0100

    A/b test basis
    one with text and one without

--- Assets/Scripts/Magnifier.cs
+++ Assets/Scripts/Magnifier.cs
@@ -1,24 +1,54 @@
 using UnityEngine;
+using TMPro;
 
 public class Magnifier : MonoBehaviour
 {
-
     public RectTransform magnifierUI;
-    public Camera zoomCamera; 
+    public Camera zoomCamera;
+    public TMP_Text clueTextUI;
+
+    Clue currentClue;
 
     void Update()
     {
-        // UI follow 
+        // UI follow
         Vector2 localPos;
         RectTransformUtility.ScreenPointToLocalPointInRectangle(
-            magnifierUI.parent as RectTransform, 
-            (Vector2)Input.mousePosition, 
-            cam: null, 
+            magnifierUI.parent as RectTransform,
+            Input.mousePosition,
+            null,
             out localPos);
-            magnifierUI.anchoredPosition = localPos; 
 
-            // world follow
-            Vector3 world = Camera.main.ScreenToWorldPoint(position: new Vector3(Input.mousePosition.x, Input.mousePosition.y, z:10f));
-            zoomCamera.transform.position = new Vector3(world.x, world.y, zoomCamera.transform.position.z);
+        magnifierUI.anchoredPosition = localPos;
+
+        // world follow
+        Vector3 world = Camera.main.ScreenToWorldPoint(
+            new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));
+
+        zoomCamera.transform.position =
+            new Vector3(world.x, world.y, zoomCamera.transform.position.z);
+
+        CheckForClue();
     }
+
+   void CheckForClue()
+{
+    Vector2 mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+    RaycastHit2D hit = Physics2D.Raycast(mouseWorld, Vector2.zero);
+
+    if (hit.collider != null)
+    {
+        Clue clue = hit.collider.GetComponent<Clue>();
+
+        if (clue != null)
+        {
+            clueTextUI.text = clue.clueText;
+            clueTextUI.gameObject.SetActive(true);
+            return;
+        }
+    }
+
+    clueTextUI.gameObject.SetActive(false);
+}
 }
